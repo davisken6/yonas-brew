b71045734096946fd6076347dbe1284cf247e748
Kendrah <davisken6@yahoo.com>
2021-10-21 01:35:43 -0400

    dc1.js

---
+++ dc1.js
@@ -0,0 +1,12 @@
+Are you at least 21 years of age?
+
+
+<button onclick="myFunction()">Yes</button>
+<button onclick="myFunction()">No</button>
+
+<script>
+    function myFunction() {
+        confirm("Welcome!");
+    }
+
+</script>
