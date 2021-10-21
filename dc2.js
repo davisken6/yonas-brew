bd020073b1ae6895a9b617039075b7bff6fc5fb7
Kendrah <davisken6@yahoo.com>
2021-10-21 01:36:16 -0400

    dc2.js

---
+++ dc2.js
@@ -0,0 +1,19 @@
+First name: <input id="first_name">
+Last name: <input id="last_name">
+    <button id="enter">Enter</button>
+
+    <hr>
+    <div id="result"></div>
+
+    <script>
+        function enter() {
+            var fname = document.getElementById('first_name').value;
+            var lname = document.getElementById('last_name').value;
+
+            var html = 'Hello <b>' + fname + '</b>' + lname;
+
+            document.getElementbyId('result').innerHTML = html;
+        }
+
+        document.getElementById('enter').addEventListener('click', enter);
+    </script>
