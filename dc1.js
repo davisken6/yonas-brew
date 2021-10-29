108e8f72d78bd07a11f3574a61f38960d6965e38
Kendrah <davisken6@yahoo.com>
2021-10-28 20:36:22 -0400

    added image

--- dc1.js
+++ dc1.js
@@ -1,6 +1,6 @@
 function userAge(){
     let is21 = prompt('Are you at least 21 years of age?');
-    if(is21 == "yes"){
+    if (is21 == "yes"){
         alert ("Welcome to my page!");
     } else {
         alert ("Check out Disney.com");
@@ -20,3 +20,28 @@ function userEmail(){
 }
 
 
+function guessingGame(){
+  let userAnswer = prompt ("Guess a number between 1 and 50");
+    console.log(userAnswer)
+
+    while (userAnswer < 6 || userAnswer > 6){
+        userAnswer = prompt("That answer is incorrect. Try again");
+    }
+   
+   if (userAnswer == "6") {
+       alert ("You got it right!");
+   } 
+}
+
+function howMany(){
+    let userNumber = prompt ("How many corgis do you want?");
+    console.log(userNumber)
+    for (let i = 0; i < userNumber; i++){
+        
+        const img = document.createElement("img");
+        img.src = "https://www.maxpixel.net/static/photo/1x/Animal-Dog-Pet-Cute-Welsh-Corgi-Pembroke-Corgi-4248916.jpg";
+        document.body.appendChild(img);
+    }
+
+
+}
